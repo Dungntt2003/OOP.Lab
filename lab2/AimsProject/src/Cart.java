dcac7e9a3f5bc5890b079fbc2444e78d3e384674
unknown <nguyenthuydung55555@gmail.com>
2023-10-28 10:00:13 +0700

    Complete section 14

--- lab2/AimsProject/src/Cart.java
+++ lab2/AimsProject/src/Cart.java
@@ -14,14 +14,15 @@ public class Cart {
             System.out.println("The cart is almost full");
         }  
     }
-    public float totalCost(){
+    public void totalCost(){
         float sum = 0;
         for (int i = 0 ; i < qtyOrdered ; i++){
             System.out.println((i+1) + "\t" + itemOrdered[i].getTitle() + "\t" + itemOrdered[i].getCost());
             sum += itemOrdered[i].getCost();
         }
-        return sum;
+        System.out.println("Total cost : " + sum);
     }
+
     int index = -1;
     public void removeDigitalVideoDisc(DigitalVideoDisc disc){
         for (int i = 0 ; i < qtyOrdered ; i++){
@@ -33,12 +34,31 @@ public class Cart {
             System.out.println("Not found DVD in cart");
             return;
         }
-        DigitalVideoDisc newArray[] = new DigitalVideoDisc[qtyOrdered-1];
+        DigitalVideoDisc newArray[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
         System.arraycopy(itemOrdered, 0 , newArray, 0 , index);
         System.arraycopy(itemOrdered, index+1 , newArray, index, qtyOrdered - index - 1);
         itemOrdered = newArray;
         qtyOrdered --;
         System.out.println("The disc has been removed");
     }
-    
+
+    //Truyền tham số theo mảng biến số (variable arguments array)
+    public void addDigitalVideoDisc(DigitalVideoDisc... dvdList){
+        for (int i = 0 ; i < dvdList.length ; i++){
+            this.addDigitalVideoDisc(dvdList[i]);
+        }
+    }
+
+    // Truyền tham số là mảng
+    // public void addDigitalVideoDisc(DigitalVideoDisc[] list){
+    //     for (int i = 0 ; i < list.length ; i++){
+    //         this.addDigitalVideoDisc(list[i]);
+    //     }
+    // }
+
+    // Truyền tham số khác với các hàm trên
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1,DigitalVideoDisc dvd2){
+        this.addDigitalVideoDisc(dvd1);
+        this.addDigitalVideoDisc(dvd2);
+    }
 }
