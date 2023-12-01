0aeaffe89503a024d6b9e03b28b9d94b6f64f16f
unknown <nguyenthuydung55555@gmail.com>
2023-12-02 00:17:43 +0700

    Add store screen

--- AimsProject/src/hust/soict/hedspi/aims/media/CompactDisc.java
+++ AimsProject/src/hust/soict/hedspi/aims/media/CompactDisc.java
@@ -2,39 +2,40 @@ package hust.soict.hedspi.aims.media;
 
 import java.util.ArrayList;
 
-
-public class CompactDisc extends Disc{
+public class CompactDisc extends Disc {
 	private String artist;
 	private ArrayList<Track> tracks = new ArrayList<Track>();
-	
+
 	public CompactDisc() {
 		// TODO Auto-generated constructor stub
 	}
+
 	public CompactDisc(String title, String category, float cost, int length, String director, String artist) {
 		// TODO Auto-generated method stub
 		super(title, category, cost, length, director);
-		this.artist = artist;	
+		this.artist = artist;
 	}
-	
+
 	public String getArtist() {
 		return artist;
 	}
-	
+
 	public void addTrack(Track track) {
 		int index = tracks.indexOf(track);
 		if (index != -1) {
 			System.out.println("Track " + track.getTitle() + " has been added");
-		}
-		else tracks.add(track);
+		} else
+			tracks.add(track);
 	}
-	
+
 	public void removeTrack(Track track) {
 		int index = tracks.indexOf(track);
 		if (index != -1) {
 			tracks.remove(index);
-		}
-		else System.out.println("Track " + track.getTitle() + " not found");
+		} else
+			System.out.println("Track " + track.getTitle() + " not found");
 	}
+
 	public int getLength() {
 		int sumLength = 0;
 		for (Track track : tracks) {
@@ -42,19 +43,22 @@ public class CompactDisc extends Disc{
 		}
 		return sumLength;
 	}
-	public void play() {
+
+	public String play() {
 		// TODO Auto-generated method stub
-		for (Track track : tracks) {
-			track.play();
-		}
+		return printAllTracks();
 	}
-	public void printAllTracks() {
+
+	public String printAllTracks() {
+		String s = "";
 		for (Track track : tracks) {
-			System.out.println(track.getTitle() + " - " + track.getLength());
+			s += track.getTitle() + " " + track.getLength() + "\n";
 		}
+		return s;
 	}
+
 	public String toString() {
 		return super.tostring() + " - " + this.artist;
 	}
-	
+
 }
